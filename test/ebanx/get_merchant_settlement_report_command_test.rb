ecebcc348028c62e930440f54c32f423cc8b20d0
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2016-10-28 16:18:59 -0200

    empty line

--- test/ebanx/get_merchant_settlement_report_command_test.rb
+++ test/ebanx/get_merchant_settlement_report_command_test.rb
@@ -2,7 +2,7 @@ require_relative '../test_helper'
 
 describe Ebanx::Command::GetMerchantSettlementReport do
   it 'performs a successful settlement report' do
-    params = { settlement_id: 100 }
+    params = { settlement_id: 33 }
     response = ebanx.do_get_merchant_settlement_report params
     response.http_code.must_equal 200
     response.response['status'].must_equal 'SUCCESS'
