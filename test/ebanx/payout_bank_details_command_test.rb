a5efdc931598bc7019c1c5d36648a8a38628d865
Vinicius Ferreira <vinicius.ferreira@ebanx.com>
2017-02-13 19:17:44 -0200

    Add new line at the end of the files

--- test/ebanx/payout_bank_details_command_test.rb
+++ test/ebanx/payout_bank_details_command_test.rb
@@ -1,10 +1,8 @@
 require_relative '../test_helper'
 
 describe Ebanx::Command::PayoutBankDetails do
-
   it "performs a successful" do
     response = ebanx.do_payout_bank_details
     response.http_code.must_equal 200
   end
-
 end
--- test/ebanx/simulate_payout_command_test.rb
+++ test/ebanx/simulate_payout_command_test.rb
@@ -4,12 +4,10 @@ describe Ebanx::Command::PayoutSimulate do
   it "can't run without arguments" do
     lambda { ebanx.do_payout_simulate }.must_raise ArgumentError
   end
-
   it "performs a successful" do
     params = { amount: 10.0, country: 'br', currency_code: 'USD' }
     response = ebanx.do_payout_simulate params
     response.http_code.must_equal 200
     response.response['type'].must_equal 'success'
   end
-
 end
